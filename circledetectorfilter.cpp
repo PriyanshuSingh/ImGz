d0f31961923c3e54de29ef7549d7adb9208745a9
PriyanshuSingh <priyanshusingh05101994@gmail.com>
2015-07-21 17:55:16 +0530

    circle Detector added.

---
+++ circledetectorfilter.cpp
@@ -0,0 +1,7 @@
+#include "circledetectorfilter.h"
+
+CircleDetectorFilter::CircleDetectorFilter()
+{
+
+}
+
---
+++ circledetectorfilter.h
@@ -0,0 +1,11 @@
+#ifndef CIRCLEDETECTORFILTER_H
+#define CIRCLEDETECTORFILTER_H
+
+
+class CircleDetectorFilter : public Filter
+{
+public:
+    CircleDetectorFilter();
+};
+
+#endif // CIRCLEDETECTORFILTER_H
